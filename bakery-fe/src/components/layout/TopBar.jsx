e70c6ee00867b2e2b39152aa87d579bd64c19055
h-i-e-u <hieudoanw11@gmail.com>
2025-06-18 19:25:25 +0700

    feat: add @iconify/react for icon support and update TopBar component

--- bakery-fe/src/components/layout/TopBar.jsx
+++ bakery-fe/src/components/layout/TopBar.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { Icon } from '@iconify/react';
+
 
 const TopBar = () => {
   const navigate = useNavigate()
@@ -9,9 +10,7 @@ const TopBar = () => {
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h8m-8 6h16" />
-            </svg>
+            <Icon icon="mdi:menu" className="text-2xl" />
           </label>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
             <li><Link to="/">Home</Link></li>
