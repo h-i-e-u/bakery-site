184a34796da1b66a2e0d3346da4e3326e1c84651
h-i-e-u <hieudoanw11@gmail.com>
2025-06-18 19:33:58 +0700

    refactor: clean up TopBar component structure and improve readability

--- bakery-fe/src/components/layout/TopBar.jsx
+++ bakery-fe/src/components/layout/TopBar.jsx
@@ -5,42 +5,40 @@ import { Icon } from '@iconify/react';
 const TopBar = () => {
   const navigate = useNavigate()
 
-  return (
+return (
     <header className="navbar bg-base-100 shadow-md">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <Icon icon="mdi:menu" className="text-2xl" />
-          </label>
-          <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/shop">Shop</Link></li>
-            <li><Link to="/about">About</Link></li>
-          </ul>
+        <div className="navbar-start lg:hidden">
+            <div className="dropdown">
+                <label tabIndex={0} className="btn btn-primary btn-outline">
+                    <Icon icon="mdi:menu" className="text-2xl" />
+                </label>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/shop">Shop</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+            </div>
+            <Link to="/" className="btn btn-ghost normal-case text-xl font-amaranth">Sweet Delights</Link>
         </div>
-        <Link to="/" className="btn btn-ghost normal-case text-xl font-amaranth">Sweet Delights</Link>
-      </div>
-      
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/shop">Shop</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </div>
-      
-      <div className="navbar-end">
-        <button className="btn btn-ghost btn-circle" onClick={() => navigate('/cart')}>
-          <div className="indicator">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            <span className="badge badge-sm indicator-item">0</span>
-          </div>
-        </button>
-      </div>
+        
+        <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/shop">Shop</Link></li>
+                <li><Link to="/about">About</Link></li>
+            </ul>
+        </div>
+        
+        <div className="navbar-end">
+            <button className="btn btn-ghost btn-circle" onClick={() => navigate('/cart')}>
+                <div className="indicator">
+                    <Icon icon="mdi:cart-outline" className="h-5 w-5" />
+                    <span className="badge badge-sm indicator-item">0</span>
+                </div>
+            </button>
+        </div>
     </header>
-  )
+)
 }
 
 export default TopBar
