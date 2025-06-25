88d20beb40ee3b6e88b73a0d716820ecd72979c6
h-i-e-u <hieudoanw11@gmail.com>
2025-06-25 14:18:15 +0700

    refactor: remove React import from component files for cleaner code

--- bakery-fe/src/pages/admin/AddProduct.jsx
+++ bakery-fe/src/pages/admin/AddProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import api from "../../api";
 import LeftBar from "../../components/layout/LeftBar";
 import { useNavigate } from "react-router-dom";
